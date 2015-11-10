54a1a48e4ff18282bde6533528c6b8580e4aac73
bhecox <bill@hecox.net>
2015-11-10 13:28:30 -0600

    first branch commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# samples-android-emdk
+SimulScanSample1
---
+++ SimulScanSample1/src/com/symbol/emdk/simulscansample1/Settings.java
@@ -0,0 +1,17 @@
+package com.symbol.emdk.simulscansample1;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Settings {
+	int selectedFileIndex = 0;
+	ArrayList<File> fileList;
+	public Boolean enableAutoCapture;
+	public Boolean enableDebugMode;
+	public Boolean enableFeedbackAudio;
+	public Boolean enableHaptic;
+	public Boolean enableLED;
+	public Boolean enableResultConfirmation;
+	public int identificationTimeout;
+	public int processingTimeout;
+}
